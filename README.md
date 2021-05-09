99c36797b0bfd9c577f47288e90a794e76788878
tgstoecker <tyll.stoecker@gmail.com>
2021-05-10 00:23:13 +0200

    first commit ;D

---
+++ README.md
@@ -0,0 +1,8 @@
+# Rust Fun
+## My personal collection of learning material, small projects and useful links for all things Rust.
+
+### Links to "official" learning and training:
+
+### Third party learning and training:
+
+### Links to youtube tutorials:
---
+++ cs50_like/mario_easy/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    for n in 1..=10 {
+        print!("#");
+    }
+    println!("");
+}
+
+
+//Printing a character a variable number of times with println
+//https://stackoverflow.com/questions/35280798/printing-a-character-a-variable-number-of-times-with-println
+
+//fn give_love(count: usize) {
+//    println!("Here is love for you: {:♥<1$}", "", count);
+//}
+
